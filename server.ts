df83a0b767fbf07ca604ddf4ef72c16f63b7aa48
Radames Ramirez <rramirez31@ucol.mx>
2022-08-31 13:21:25 -0500

    Adding dependences

--- server.ts
+++ server.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
 import db from "./src/config/sequelize";
-
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+db.sync();
+dotenv.config();
 
 const port = process.env.port ?? 8000;
-db.sync();
 const server = app.listen(port, () => {
     console.log(`Servidor a la escucha. Puerto: ${port}`);
 });
